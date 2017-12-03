a65876de3374ae64597109bcb554bc0d91a3ffbf
alexwalland <beastdrogba@gmail.com>
2017-12-03 00:12:31 +0000

    Added clouds
    spawn clouds and they move in the background note- need to fix enemies movement shield not stopping bullets and the confines of the level based on scaling in bigger screens

---
+++ Prototype/Assets/Scripts/MoveClouds.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveClouds : MonoBehaviour {
+
+	public float speed;
+
+	Rigidbody CRB;
+
+	// Use this for initialization
+	void Start () {
+		speed = -5;
+		CRB = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		CRB.velocity = new Vector3 (speed, 0, 0);
+	}
+}
