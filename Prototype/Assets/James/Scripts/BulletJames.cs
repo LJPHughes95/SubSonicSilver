334c6bef4f2acbf952747b721dbe4f655b33ffab
LJPHughes95 <u1459272@unimail.hud.ac.uk>
2017-12-12 20:11:05 +0000

    James Merge Fixes

---
+++ Prototype/Assets/James/Scripts/BulletJames.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletJames : MonoBehaviour {
+
+
+    public int projDirection;
+    public float speed;
+
+    public GameObject ExplosionGo;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        projDirection = PlayerController.playerInstance.GetDirection();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position += new Vector3(projDirection * speed, 0.0f, 0.0f);
+
+        Destroy(gameObject, 2.0f);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "enemy")
+        {
+            PlayExplosion();
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    void PlayExplosion()
+    {
+        GameObject explosion = (GameObject)Instantiate(ExplosionGo);
+
+        //plays the explosion where the bullet touches the enemy
+        explosion.transform.position = transform.position;
+    }
+
+    /*void OnCollisionEnter (Collision collision)
+	{
+		if (collision.gameObject.tag == "Boss" && health <= 0){
+			explosion.ChangeImage();
+			Destroy (collision.gameObject);
+			Destroy (gameObject);
+		}
+	}
+	*/
+}
