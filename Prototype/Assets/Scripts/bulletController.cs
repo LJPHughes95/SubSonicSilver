899321d7afc0916d24be091e22f7c301c9148c95
LJPHughes95 <u1459272@unimail.hud.ac.uk>
2017-12-13 16:53:07 +0000

    Revert "Merge pull request #18 from LJPHughes95/Liam"
    This reverts commit cca6d0fe1e127eb9b2ebbdc67cf37a0365a565c4, reversing
    changes made to 1ef61f318a179c78079e0f62c7974f2a45060617.

---
+++ Prototype/Assets/Scripts/bulletController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletController : MonoBehaviour {
+
+	public float speed;
+
+	Rigidbody bullet;
+
+	public GameObject ExplosionGo;
+
+	// Use this for initialization
+	void Start () {
+		speed = 50f;
+		bullet = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+       
+		bullet.velocity = new Vector3( speed, 0, 0);
+        Destroy(gameObject, 2.0f);
+	}
+
+	void OnCollisionEnter (Collision collision)
+	{
+
+		if (collision.gameObject.tag == "enemy") {
+			PlayExplosion();
+			Destroy (collision.gameObject);
+			Destroy (gameObject);
+		}
+		if (collision.gameObject.tag == "Boss" && BossController.health <= 0){
+			PlayExplosion();
+			Destroy (collision.gameObject);
+			Destroy (gameObject);
+		}
+		if (collision.gameObject.tag == "turret")
+		{
+			PlayExplosion();
+			Destroy (gameObject);
+		}
+	}
+
+	void PlayExplosion()
+	{
+		GameObject explosion = (GameObject)Instantiate(ExplosionGo);
+		explosion.transform.position = transform.position;
+	}
+}
---
+++ Prototype/Assets/bossTurret.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bossTurret : MonoBehaviour {
+
+	public Transform emitter;
+
+	public Rigidbody bullet;
+
+	public int health;
+
+	public float fireRate;
+
+	float timer;
+	int i;
+
+	// Use this for initialization
+	void Start () {
+		health = 5;
+		i = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer > fireRate)
+		{
+			Instantiate (bullet, emitter.position, Quaternion.identity);
+			timer = 0;
+		}
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.tag == "bullet") {
+			if (health == 1) {
+
+			} else {
+				health--;
+				Invoke ("hit", 0.2f);
+			}
+		}
+	}
+		
+	public void hit()
+	{
+		GetComponent<MeshRenderer> ().enabled = false;
+		Invoke ("reset", 0.2f);
+	}
+
+	public void reset()
+	{
+		if (i < 2) {
+			GetComponent<MeshRenderer> ().enabled = true;
+			i += 1;
+			Invoke ("hit", 0.2f);
+		} else {
+			GetComponent<MeshRenderer> ().enabled = true;
+			i = 0;
+		}
+	}
+}
