f229ae7eda6c6565709ee63594782f142f674e79
LJPHughes95 <u1459272@unimail.hud.ac.uk>
2017-12-15 09:32:14 +0000

    a

---
+++ Prototype/Assets/BossCheck.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BossCheck : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (GameObject.FindGameObjectWithTag ("Boss") == null)
+		{
+			SceneManager.LoadScene (0);
+		} 
+	}
+}
---
+++ Prototype/Assets/Scripts/bossTurret.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bossTurret : MonoBehaviour {
+
+	public Transform emitter;
+
+	public Rigidbody bullet;
+
+	public int health;
+
+	public float fireRate;
+
+	float timer;
+	int i;
+
+	// Use this for initialization
+	void Start () {
+		health = 2;
+		i = 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer > fireRate)
+		{
+			Instantiate (bullet, emitter.position, Quaternion.identity);
+			timer = 0;
+		}
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.tag == "pBullet") {
+			if (health == 0) {
+				Destroy (gameObject);
+				BossController.baseBossHealth -= 0.1f;
+			} else {
+				health--;
+				Invoke ("hit", 0.2f);
+				BossController.baseBossHealth -= 0.1f;
+			}
+		}
+	}
+
+	public void hit()
+	{
+		GetComponent<MeshRenderer> ().enabled = false;
+		Invoke ("reset", 0.2f);
+	}
+
+	public void reset()
+	{
+		if (i < 2) {
+			GetComponent<MeshRenderer> ().enabled = true;
+			i += 1;
+			Invoke ("hit", 0.2f);
+		} else {
+			GetComponent<MeshRenderer> ().enabled = true;
+			i = 0;
+		}
+	}
+}
